a16f1de669a98784f5c02671edf2c2ed82d36c34
Ataberk Kasap <c.ataberkkasap@hotmail.com>
2021-10-19 00:05:37 +0300

    18 Ekim Ders Sonu

---
+++ src/app.js
@@ -0,0 +1,18 @@
+// console.log("merhaba kodlama.io") Gamze Nur Arslanboğa#1193 
+
+let dolarDun = 9.20;
+let dolarBugun = 9.30;
+
+// console.log(dolarDun);
+
+const euroDun = 11.2;
+
+//array
+//camelCasing
+let konutKredileri = ["Konut Kredisi", "Emlak Konut Kredisi", "Kamu Konut Kredisi"];
+
+// console.log(konutKredileri);
+
+for (let i = 0; i < konutKredileri.length; i++){
+	console.log("<li>" + konutKredileri[i] + "</li>");
+}
