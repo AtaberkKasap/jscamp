cef58e2ee3c32f27886d82a61532204c0f883b93
Ataberk Kasap <c.ataberkkasap@hotmail.com>
2021-11-09 00:17:02 +0300

    8 Kasım Ders Sonu (3. gün)

---
+++ src/components/userComponent.js
@@ -0,0 +1,27 @@
+import { BaseLogger, ElasticLogger, MongoLogger } from "../crossCuttingConcerns/logging/logger.js";
+import User from "../models/user.js";
+import UserService from "../services/userService.js";
+
+console.log("User component yüklendi.");
+
+let logger1 = new ElasticLogger();
+let userService = new UserService(logger1);
+
+let user1 = new User(1, "Ataberk", "Kasap", "İzmir");
+let user2 = new User(2, "Engin", "Demiroğ", "Ankara");
+userService.add(user1);
+userService.add(user2);
+
+
+console.log(userService.list());
+
+console.log(userService.getById(2));
+
+
+/*
+let customer = {id: 1, firstName: "Engin"};
+
+//prototyping
+customer.lastName = "Demiroğ";
+
+console.log(customer.lastName);*/
