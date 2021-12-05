ef29f5d2df89df2d5132b4cf90c262daf14baecb
Ataberk Kasap <c.ataberkkasap@hotmail.com>
2021-12-05 13:26:50 +0300

    4. gün ders sonu

--- src/components/userComponent.js
+++ src/components/userComponent.js
@@ -1,4 +1,5 @@
 import { BaseLogger, ElasticLogger, MongoLogger } from "../crossCuttingConcerns/logging/logger.js";
+import Customer from "../models/customer.js";
 import User from "../models/user.js";
 import UserService from "../services/userService.js";
 
@@ -9,13 +10,13 @@ let userService = new UserService(logger1);
 
 let user1 = new User(1, "Ataberk", "Kasap", "İzmir");
 let user2 = new User(2, "Engin", "Demiroğ", "Ankara");
-userService.add(user1);
-userService.add(user2);
+// userService.add(user1);
+// userService.add(user2);
 
 
-console.log(userService.list());
+// console.log(userService.list());
 
-console.log(userService.getById(2));
+// console.log(userService.getById(2));
 
 
 /*
@@ -25,3 +26,13 @@ let customer = {id: 1, firstName: "Engin"};
 customer.lastName = "Demiroğ";
 
 console.log(customer.lastName);*/
+console.log("--------------------------")
+userService.load();
+
+let customerToAdd = new Customer(1, "Seda", "Yılmaz", "Ankara", 21);
+customerToAdd.type = "customer";
+userService.add(customerToAdd);
+console.log("Customers", userService.customers);
+console.log("Employees", userService.employees);
+console.log("Errors", userService.errors);
+console.log(userService.getCustomersSorted());
